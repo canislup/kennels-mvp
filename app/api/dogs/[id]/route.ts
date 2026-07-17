d2a4ab7acce9846229f6c79416384cbef0d8ffec
Pedro Vasconcelos <pedrovasconcelos@Pedros-MacBook-Pro.local>
2026-07-17 14:21:38 -0300

    CRUD operations finalized

--- app/api/dogs/[id]/route.ts
+++ app/api/dogs/[id]/route.ts
@@ -115,8 +115,66 @@ export const PATCH = async (request: NextRequest, { params } : { params: Promise
 
         console.log("dog", dog);
 
+        // 8) Return updated dog data
         return NextResponse.json({message: "Dog updated successfully.", data: updatedDogData}, {status: 200})
 
+    } catch (error) {
+        console.error("❌ Something went wrong: ", error);
+        return NextResponse.json(
+            { message: "Internal Server Error" },
+            { status: 500 }
+        );
+    }
+}
+
+export const DELETE = async (request: NextRequest, { params } : { params: Promise<{id: string}> }) => {
+    try {
+        // 1) Await the params
+        const resolvedParams = await params;
+        const dogId = resolvedParams.id;
+
+        console.log("Unwrapped Dog ID: ", dogId);
+
+        // 3) Authenticate Session
+        const payload = await verifyKennelSession();
+        if (!payload) {
+            return NextResponse.json(
+                {message: "Unauthorized."},
+                {status: 401}
+            )
+        }
+
+        // 4) Extract the id of the owner account
+        const ownerId = payload.userID as string;
+
+        // 5) Retrieve dog data from database
+        const dog = await prisma.dog.findUnique({
+            where: {
+                id: dogId,
+            },
+            include: {
+                kennel: true,
+            }
+        })
+
+        // 6) Verify if dog belongs to owner account
+        if(!dog || dog.kennel?.ownerId !== ownerId) {
+            return NextResponse.json(
+                {message: "Dog not found."},
+                {status: 404}
+            )
+        }
+
+        // Delete the selected dog profile
+        const deletedDog = await prisma.dog.delete({
+            where:{
+                id: dogId,
+            }
+        })
+
+        console.log("Dog deleted: ", deletedDog);
+
+        return NextResponse.json({message: "Dog deleted successfully.", data: deletedDog}, {status: 200});
     } catch (error) {
         console.error("❌ Something went wrong: ", error);
         return NextResponse.json(
